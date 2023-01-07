5ca6d02f3d4619444079433911c47300d092f1e4
Hugo Lesta <hugo.lesta@bestseller.com>
2023-01-07 19:28:16 +0100

    fix: Fixes import cycle

--- gitguardian/resource_create_team.go
+++ gitguardian/resource_create_team.go
@@ -3,14 +3,12 @@ package gitguardian
 import (
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/Gaardsholt/go-gitguardian/client"
 	"github.com/Gaardsholt/go-gitguardian/teams"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hugolesta/terraform-provider-gitguardian/gitguardian"
 )
 
 func resourceCreateTeam() *schema.Resource {
@@ -63,39 +61,6 @@ type Team struct {
 	TeamId int `json:"team_id"`
 	TeamPermission string `json:"team_permission"`
 	IncidentPermission string `json:"incident_permission"`
-}
-
-func buildTeamStruct(d *schema.ResourceData) *teams.TeamGetResult {
-
-	team := &gitguardian.Team{
-		Id:  strings.TrimSpace(d.Get("team_id").(string)),
-		MemberId:  strings.TrimSpace(d.Get("member_id").(string)),
-		TeamId:  strings.TrimSpace(d.Get("team_id").(string)),
-		TeamPermission:  strings.TrimSpace(d.Get("team_permission").(string)),
-		IncidentPermission:  strings.TrimSpace(d.Get("incident_permission").(string)),
-
-	}
-
-	if attr, ok := d.GetOk("id"); ok {
-		team.Id = attr.(int)
-	}
-
-	if attr, ok := d.GetOk("member_id"); ok {
-		team.MemberId = attr.(int)
-	}
-
-	if attr, ok := d.GetOk("team_id"); ok {
-		team.TeamId = attr.(int)
-	}
-
-	if attr, ok := d.GetOk("team_permission"); ok {
-		team.TeamPermissions = attr.(string)
-	}
-
-	if attr, ok := d.GetOk("incident_permission"); ok {
-		team.IncidentPermission = attr.(string)
-	}
-	return team
 }
 
 func resourceCreateTeamRead(d *schema.ResourceData, m interface{}) error {
